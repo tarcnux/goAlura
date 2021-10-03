8cf1877ba993319e255837a92c3febc2b578d0f4
Tarcisio Nunes <github@tarcnux.com.br>
2021-10-03 13:54:42 -0300

    Código inicial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # goAlura
 Golang básico
+
+Clonar o repositório: [goAlura](https://github.com/tarcnux/goAlura.git)
+
+$ cd goAlura
+$ go run monitor.go
+
---
+++ monitor.go
@@ -0,0 +1,16 @@
+//monitor.go
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Tarcísio"
+	versao := 1.1
+
+	fmt.Println("Olá, sr(a).", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
