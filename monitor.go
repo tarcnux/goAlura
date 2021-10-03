f9b9f8c664b57ae0e176bee153ab8a8c7a1f466a
Tarcisio Nunes <github@tarcnux.com.br>
2021-10-03 15:59:05 -0300

    Loop infinito com For

--- monitor.go
+++ monitor.go
@@ -9,21 +9,23 @@ import (
 
 func main() {
 
-	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
+	for {
+		exibeIntroducao()
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 
 	/**
