2f01a94348909dd3ac22851581341e8721c6831d
Tarcisio Nunes <github@tarcnux.com.br>
2021-10-03 23:23:35 -0300

    Registro de Logs - escrvendo em arquivo TXT

--- monitor.go
+++ monitor.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -24,9 +25,11 @@ func main() {
 
 		switch comando {
 		case 1:
+			fmt.Println("Iniciando o Monitoramento")
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -145,8 +148,18 @@ func registraLog(site string, status bool) {
 		fmt.Print(err)
 	}
 
-	arquivo.WriteString(site + " online: " + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + site + " online: " + strconv.FormatBool(status) + "\n")
 
 	//fmt.Println(arquivo)
 	arquivo.Close()
+}
+
+func imprimeLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println(string(arquivo))
 }
